214875bca0e24da1e015786e74200925d9f1b42b
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-20 16:07:58 -0300

    adding cors connection and listening socket connection

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
 import "dotenv/config";
 import express from "express";
-import { router } from "./routes"
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+import { router } from "./routes";
 
 const app = express();
-app.use(express.json())
+app.use(cors())
 
-app.use(router)
+const serverHttp = http.createServer(app)
+
+//permite a conexão de qualquer origem
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+})
+
+//Ficará "ouvindo" quantos usuários estão conectados no socket
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
+
+app.use(express.json());
+
+app.use(router);
 
 app.get("/github", (request, response) => {
-  response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
-})
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
 app.get("/signin/callback", (request, response) => {
   const { code } = request.query;
 
-  return response.json(code)
-})
+  return response.json(code);
+});
 
-app.listen(4000, () => console.log(`Server is running on Port 4000`))
+export { serverHttp, io }
