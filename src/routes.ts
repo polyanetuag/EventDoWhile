ba2de5db3821f2066dcaa02ebae4e9fe6dc41fc7
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-20 16:31:40 -0300

    route user profile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./Controllers/AuthenticateUserController";
 import { CreateMessageController } from "./Controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./Controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./Controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
@@ -13,5 +14,6 @@ router.post(
   new CreateMessageController().handle
 );
 router.get("/messages/last3", new GetLast3MessagesController().handle);
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };
